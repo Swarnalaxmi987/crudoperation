64b8ffc2bc0a05b54e6eba72cc9441788ac96bd3
Swarnalaxmi987 <132799744+Swarnalaxmi987@users.noreply.github.com>
2023-05-07 09:57:38 +0530

    Add files via upload

---
+++ crudoper/user.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container my-5">
+    <form method="post">
+
+  <div class="mb-3">
+    <label>Name</label>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off">
+  </div>
+
+  <div class="mb-3">
+    <label>Email</label>
+    <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off">
+  </div>
+
+  <div class="mb-3">
+    <label>Mobile</label>
+    <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off">
+  </div>
+
+  <div class="mb-3">
+    <label>Password</label>
+    <input type="Password" class="form-control" placeholder="Enter your password" name="password" autocomplete="off">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+
+  </body>
+</html>
